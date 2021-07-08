7c26a3416716ffbc4d86a172279e616159753ab9
g-n-x <gnx@tuta.io>
2021-07-07 21:18:59 -0300

    implement fib function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let ceiling: u32 = 400000;
+    println!("print fib numbers below {}", ceiling);
+    fib(ceiling);
+}
+
+fn fib(ceil: u32) {
+    let (mut a, mut b, mut c): (u32, u32, u32) = (0, 1, 0);
+    while c <= ceil {
+        println!("{}", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
 }
